b1dd5093ccc7d23ebc53d50a5e3658a0112fe347
Alesandro Diogo <alesandromendesdiogo24@gmail.com>
2023-09-12 16:30:18 +0100

    Database set and create model Pessoa

--- CRUDAPI/Program.cs
+++ CRUDAPI/Program.cs
@@ -1,3 +1,6 @@
+using CRUDAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<Contexto>(opcoes => 
+opcoes.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoDb")));
 
 var app = builder.Build();
 
